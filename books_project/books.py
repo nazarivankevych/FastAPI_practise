8df76193f6696299a18d3a7831d7b09b67288b71
nazarivankevych <nazar.ivankevych@gmail.com>
2023-05-19 21:21:11 +0300

    Combine in get title, author, category, and create a simple POST request

--- books_project/books.py
+++ books_project/books.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi import Body
 
 from constants import BOOKS
 
@@ -10,11 +11,11 @@ async def read_all_books():
     return BOOKS
 
 
-@app.get("/books/{book_title}")
-async def read_books(book_title: str):
-    for book in BOOKS:
-        if book.get("title").casefold() == book_title.casefold():
-            return book
+# @app.get("/books/{book_title}")
+# async def read_books(book_title: str):
+#     for book in BOOKS:
+#         if book.get("title").casefold() == book_title.casefold():
+#             return book
 
 
 # @app.get("/books/")
@@ -26,12 +27,18 @@ async def read_books(book_title: str):
 #     return books_to_return
 
 
-@app.get("/books/{book_author}/")
-async def read_author_category_by_query(book_author: str, category: str):
+@app.get("/books/{book_to_return}/")
+async def read_author_category_by_query(book_title: str, book_author: str, book_category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() or \
-                book.get('category').casefold() == category.casefold():
+        if book.get('title').casefold() == book_title.casefold() or \
+            book.get('author').casefold() == book_author.casefold() or \
+                book.get('category').casefold() == book_category.casefold():
             books_to_return.append(book)
 
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
