3bb31d021e55ccc8544fbedca832db0c9b7cda8c
nazarivankevych <nazar.ivankevych@gmail.com>
2023-05-19 22:26:04 +0300

    remove not needed files, add post, put and delete requests

--- books_project/books.py
+++ books_project/books.py
@@ -42,3 +42,18 @@ async def read_author_category_by_query(book_title: str, book_author: str, book_
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for index in range(len(BOOKS)):
+        if BOOKS[index].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[index] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
