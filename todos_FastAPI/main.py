dffef1d78d3ca15c69d1d2b7db40f171b4cd172a
nazarivankevych <nazar.ivankevych@gmail.com>
2023-06-01 12:39:42 +0300

    Add router users for changing password for admin and another users, create a validator for that task

--- todos_FastAPI/main.py
+++ todos_FastAPI/main.py
@@ -9,7 +9,7 @@ from fastapi import FastAPI
 
 import models
 from database import engine
-from routers import auth, todos, admin
+from routers import auth, todos, admin, users
 
 
 app = FastAPI()
@@ -19,3 +19,4 @@ app = FastAPI()
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
