462eb9e670e96029ea49f89b865a25d68083031b
nazarivankevych <nazar.ivankevych@gmail.com>
2023-06-05 15:26:26 +0300

    Set up a connection to mesql database, authentication, registration, create APIs for every templates

--- todo-FullStack/TodoApp/database.py
+++ todo-FullStack/TodoApp/database.py
@@ -1,13 +1,21 @@
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
-#
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-#
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
-#
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#
-# Base = declarative_base()
+
+
+# MYSQL Series
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:8080/todoapp"
+
+# MYSQL Series
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
--- todo-FullStack/TodoApp/main.py
+++ todo-FullStack/TodoApp/main.py
@@ -1,15 +1,14 @@
-from fastapi import FastAPI
-# from database import engine
-from starlette.staticfiles import StaticFiles
-
+from fastapi import FastAPI, Depends
+import models
+from database import engine
 from routers import auth, todos
-# import models
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# app.include_router(auth.router)
+app.include_router(auth.router)
 app.include_router(todos.router)
